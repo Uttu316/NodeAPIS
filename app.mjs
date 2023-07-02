df64067613365c1b4af49fa88c6c2d2bbff75bc3
Utkarsh Gupta <utkarsh.gupta1@housing.com>
2023-07-03 03:09:32 +0530

    Add routes

--- app.mjs
+++ app.mjs
@@ -9,11 +9,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 
+
 app.get("/", (req, res) => {
   res.json({ data: "Server running" });
 });
 
-app.use("/api", userService);
+app.use("/", userService);
 
 app.use("/docs", ...swaggerConfig);
 
